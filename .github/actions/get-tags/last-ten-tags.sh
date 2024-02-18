d23b8c255a426a93a9f99baa5080d90ed6ff7916
Emily Washer <emily.washer@hellofresh.com.au>
2024-02-19 10:46:17 +1100

    trying no brakcets:

--- .github/actions/get-tags/last-ten-tags.sh
+++ .github/actions/get-tags/last-ten-tags.sh
@@ -3,7 +3,7 @@
 set -e -f
 set -x -o pipefail
 
-VERSION=${{ (git tag -l --sort=-v:refname | head -n 1) }}
-TAGS=${{ (git for-each-ref --format="%(taggerdate) | %(refname) | %(contents:subject)" --sort=-taggerdate --count=10 refs/tags) }}
+VERSION=$(git tag -l --sort=-v:refname | head -n 1)
+TAGS=$(git for-each-ref --format="%(taggerdate) | %(refname) | %(contents:subject)" --sort=-taggerdate --count=10 refs/tags)
 echo "${TAGS}"
-#echo "${VERSION}"
+echo "$VERSION"
