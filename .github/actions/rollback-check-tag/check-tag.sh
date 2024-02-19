cdeb5b05e1808aa2a9a15db6ebdc495fc0fb0579
Emily Washer <emily.washer@hellofresh.com.au>
2024-02-20 10:56:52 +1100

    run script directly

--- .github/actions/rollback-check-tag/check-tag.sh
+++ .github/actions/rollback-check-tag/check-tag.sh
@@ -3,7 +3,7 @@
 # Regex is copied from semver.org
 REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 
-if ! (echo "${TAG}" | grep -i -E ${REGEX}); then
+if ! (echo "${1}" | grep -i -E ${REGEX}); then
 	echo "Invalid Rollback Tag: \"${TAG}\". Valid tag should match semantic versioning"
 	exit 1
 fi
