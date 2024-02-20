11a6dca6bbd1ea717acd921c3e24482e1c90982c
Emily Washer <emily.washer@hellofresh.com.au>
2024-02-20 16:44:40 +1100

    simplified regex

--- .github/actions/rollback-check-tag/check-tag.sh
+++ .github/actions/rollback-check-tag/check-tag.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 # Regex is copied from semver.org
-REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
-
-echo "${1}"
+REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$'
+#REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 
 if ! (echo "${1}" | grep -i -E ${REGEX}); then
 	echo "Invalid Rollback Tag: \"${1}\". Valid tag should match semantic versioning"
