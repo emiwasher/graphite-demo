90362dc06dd45074dab905750ddab65fe254f825
Emily Washer <emily.washer@hellofresh.com.au>
2024-02-20 16:49:42 +1100

    more double quotes

--- .github/actions/rollback-check-tag/check-tag.sh
+++ .github/actions/rollback-check-tag/check-tag.sh
@@ -4,7 +4,7 @@
 REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$'
 #REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 
-if ! (echo "${1}" | grep -i -E ${REGEX}); then
+if ! (echo "${1}" | grep -i -E "${REGEX}"); then
 	echo "Invalid Rollback Tag: \"${1}\". Valid tag should match semantic versioning"
 	exit 1
 fi
